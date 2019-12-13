690121fca84ca16f49e6a76dd3db8c2b753d87d1
sexfat <bryant6831@gmail.com>
2019-12-13 17:01:34 +0800

    add gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
 
 
 //搬家
 gulp.task('concat', function () {
     //do something
-    return gulp.src('./js/*').pipe(gulp.dest('./dist/js')) 
+    return gulp.src('./js/*').pipe(gulp.dest('./dist/js'))
 });
 
 
-gulp.task('minicss' ,function(){
-//do something
- return gulp.src('./css/plugin.css').pipe(cleanCSS({compatibility: 'ie8'})).pipe(gulp.dest('./dist/css/'))
+gulp.task('minicss', function () {
+    //do something
+    return gulp.src('./css/plugin.css').pipe(cleanCSS({
+        compatibility: 'ie8'
+    })).pipe(gulp.dest('./dist/css/'))
+});
 
-
-
-
-})
-
-
+gulp.task('sass', function () {
+    return gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
