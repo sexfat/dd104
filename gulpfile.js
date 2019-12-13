7a6d43f07df2b7a3cc9c6e56ecd0d6087af9c9a6
sexfat <bryant6831@gmail.com>
2019-12-13 16:59:04 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
 
+
+//搬家
 gulp.task('concat', function () {
     //do something
-    return gulp.src('./index.html').pipe(gulp.dest('./dist/')) 
+    return gulp.src('./js/*').pipe(gulp.dest('./dist/js')) 
 });
+
+
+gulp.task('minicss' ,function(){
+//do something
+ return gulp.src('./css/plugin.css').pipe(cleanCSS({compatibility: 'ie8'})).pipe(gulp.dest('./dist/css/'))
+
+
+
+
+})
+
+
